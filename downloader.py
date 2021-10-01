c1fc3051ff6481568ee279127816e7002c875771
Cyn1ch0 <79491635+Cyn1ch0@users.noreply.github.com>
2021-10-01 10:20:48 +0800

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -1,5 +1,6 @@
 import time
 import os
+from variables import *
 
 from bs4 import BeautifulSoup
 from selenium import webdriver
@@ -9,25 +10,35 @@ from selenium.webdriver.support.ui import Select
 
 import chromedriver_autoinstaller
 
-chromedriver_autoinstaller.install()  
 # Check if the current version of chromedriver exists
 # and if it doesn't exist, download it automatically,
 # then add chromedriver to path
+chromedriver_autoinstaller.install()  
 
+# Initialize the translations directory
+dir = os.path.join(path, trans_dir)
+if not os.path.exists(dir):
+    os.mkdir(dir)
+
+# Set the default path to download the texts in to
+# make sure it leads to the same directory as the program
 chrome_options = webdriver.ChromeOptions()
-preferences = {"download.default_directory": '/Users/drleahv/Documents/Joshua/VIS_NOV/translations',
+preferences = {"download.default_directory": path+trans_dir,
                 "directory_upgrade": True,
                 "safebrowsing.enabled": True }
 chrome_options.add_experimental_option("prefs", preferences)
 driver = webdriver.Chrome(options=chrome_options)
 driver.delete_all_cookies()
+
 # Login to law site
 driver.get('http://www.japaneselawtranslation.go.jp/law/?re=2')
 # Click on the second menu
 search_1 = driver.find_element_by_xpath("//*[contains(text(), '株式')]").click()
 time.sleep(2)
-# Pick a page among the pages of search results
-driver.find_elements_by_tag_name("a")[-6].click()
+
+# Chooses the first page of results by default.
+# The indices for the pages will be [-10 to -3]
+# driver.find_elements_by_tag_name("a")[-6].click() - Pick a page among the pages of search results. 
 while True:
     # Get the second <ul> element
     document_list = driver.find_elements_by_tag_name('ul')[1]
@@ -60,6 +71,7 @@ while True:
         obj = driver.switch_to.alert
         obj.accept()
         time.sleep(5)
+        # Do the same for english translations
         select_object.select_by_value('08')
         download_button = driver.find_elements_by_tag_name("input")[1].click()
         obj = driver.switch_to.alert
@@ -67,4 +79,5 @@ while True:
         time.sleep(5)
         driver.close()
         driver.switch_to.window(base_window)
+    # Move to the next page of search results
     driver.find_elements_by_tag_name("a")[-2].click()
